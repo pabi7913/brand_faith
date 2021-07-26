0d93ad5f838a035d42a413d1a6632b51df391075
pabi7913 <pabika790103@gmail.com>
2021-07-26 17:28:23 +0800

    v1-設定3種語言

--- js/brand_faith_0.js
+++ js/brand_faith_0.js
@@ -1,57 +1,95 @@
 // 0 
 // --------------------------------------------------
-// 設定：尋找網址
-let url = location.hash
-console.log('url', url);
-
-let lan = url.substr(1, 2)
-console.log('lan', lan);
-
-
 // 設定：滑鼠點選個別語言，網址增加#
 // 設定：改語言版本
+// 設定：下一頁的網址增加#
+// --------------------------------------------------
 $('.cn').click(function () {
-    location.href = 'brand_faith_0.html#cn'
+    location.hash = 'cn'
     $('.title').eq(0)
     .text('品牌信仰')
-    $('.title').eq(1)
+    .addClass('fadein delay05')
+    $('.title').eq(1).children().eq(0)
     .text('忠誠度大挑戰')
+    .addClass('fadein delay075')
+    $('.title').eq(1).children().eq(1)
+    .addClass('line delay10')
+    .css('margin-top','10px')
     $('.title').eq(2)
-    .text('馬上開始');
+    .text('馬上開始')
+    .addClass('fadeup delay10')
+    .attr("onclick","location='brand_faith_1.html"+location.hash+"'")
 })
 $('.en').click(function () {
-    location.href = 'brand_faith_0.html#en'
+    location.hash = 'en'
     $('.title').eq(0)
     .text('Logo Quizzes!')
+    .addClass('fadein delay05')
     .addClass('text_en')
     .css('font-weight','700')
-    $('.title').eq(1)
+    $('.title').eq(1).children().eq(0)
     .text('Will You Pass?')
+    .addClass('fadein delay075')
     .addClass('text_en')
     .css('font-weight','700')
+    $('.title').eq(1).children().eq(1)
+    .addClass('line delay10')
     $('.title').eq(2)
     .text('START')
     .addClass('text_en')
+    .addClass('fadeup delay10')
     .css('letter-spacing','0.1rem')
     .addClass('pt-1')
-    .removeClass('pb-1');
+    .removeClass('pb-1')
+    .attr("onclick","location='brand_faith_1.html"+location.hash+"'")
 })
+if ($(window).width() >= 601) {
 $('.jp').click(function () {
-    location.href = 'brand_faith_0.html#jp'
+    location.hash = 'jp'
     $('.title').eq(0)
     .text('ロゴマーク')
+    .addClass('fadein delay05')
     .css('font-weight','700')
-    $('.title').eq(1)
+    $('.title').eq(1).children().eq(0)
+    .html('ちゃんと覚えてる？')
+    .addClass('fadein delay075')
+    $('.title').eq(1).children().eq(1)
+    .addClass('line delay10')
+    .css('margin-top','10px')
+    $('.title').eq(2)
+    .text('スタート')
+    .addClass('fadeup delay10')
+    .attr("onclick","location='brand_faith_1.html"+location.hash+"'")
+})}else{
+$('.jp').click(function () {
+    location.hash = 'jp'
+    $('.title').eq(0)
+    .text('ロゴマーク')
+    .addClass('fadein delay05')
+    .css('font-weight','700')
+    $('.title').eq(1).children().eq(0)
     .html('ちゃんと<br>覚えてる？')
+    .addClass('fadein delay075')
     .css('white-space','unset')
     .css('font-size','2.75rem')
     .css('line-height','1.25')
     .css('font-weight','700')
+    $('.title').eq(1).children().eq(1)
+    .addClass('line delay10')
+    .css('margin-top','15px')
     $('.title').eq(2)
     .text('スタート')
+    .addClass('fadeup delay10')
+    .attr("onclick","location='brand_faith_1.html"+location.hash+"'")
 })
+}
+// 設定：將語系存入暫存
+// --------------------------------------------------
+localStorage.
+setItem('selected_langunage', location.hash.substr(1, 2))
 
 // 設定：滑鼠移到語言時logo動畫
+// --------------------------------------------------
 $('.button').mouseenter(function () {
     $(this).parent().prev()
         .removeClass('animate__zoomInUp')
@@ -61,15 +99,21 @@ $('.button').mouseenter(function () {
         $(this).parent().prev()
             .removeClass('animate__jello')
     })
+
 // 設定：滑鼠點選語言時關閉語言視窗
+// --------------------------------------------------
 $('.button').click(function () {
     $('.select_language')
         .removeClass('animate__bounceIn')
         .addClass('animate__zoomOut')
+        localStorage.setItem('selected_langunage', location.hash.substr(1, 2))
     setTimeout(function () {
         $('.select_language').addClass('d-none')
     }, 1000)
 })
+
+
+
 
 
 
---
+++ js/brand_faith_0_drink.js
@@ -0,0 +1,138 @@
+// 0 
+// --------------------------------------------------
+// 設定：滑鼠點選個別語言，網址增加#
+// 設定：改語言版本
+// 設定：下一頁的網址增加#
+// --------------------------------------------------
+let language = location.hash.substr(1, 2)
+switch(language){
+    default:
+    // case 'cn':
+        $('.title').eq(0)
+        .text('品牌信仰')
+
+        $('.title').eq(1).children().eq(0)
+        .text('忠誠度大挑戰')
+
+        $('.title').eq(1).children().eq(1)
+        .css('margin-top','10px')
+
+        $('.title').eq(2)
+        .text('馬上開始')
+        .attr("onclick","location='brand_faith_1_drink.html"+location.hash+"'")
+
+        $('.part')
+        .text('～飲品篇～')
+        break;
+
+    case 'en':
+        $('.title').eq(0)
+        .text('Logo Quizzes!')
+        .addClass('text_en')
+        .css('font-weight','700')
+
+        $('.title').eq(1).children().eq(0)
+        .text('Will You Pass?')
+        .addClass('text_en')
+        .css('font-weight','700')
+
+        $('.title').eq(2)
+        .text('START')
+        .addClass('text_en')
+        .css('letter-spacing','0.1rem')
+        .addClass('pt-1')
+        .removeClass('pb-1')
+        .attr("onclick","location='brand_faith_1_drink.html"+location.hash+"'")
+
+        $('.part')
+        .text('~ Beverage ~')
+        .addClass('text_en')
+        break;
+
+        case 'jp':
+            if ($(window).width() >= 601) {
+            $('.title').eq(0)
+            .text('ロゴマーク')
+            .css('font-weight','700')
+
+            $('.title').eq(1).children().eq(0)
+            .text('ちゃんと覚えてる？')
+            .css('white-space','unset')
+            .css('line-height','1.25')
+            .css('font-weight','700')
+
+            $('.title').eq(1).children().eq(1)
+            .css('margin-top','20px')
+
+            $('.title').eq(2)
+            .text('スタート')
+            .attr("onclick","location='brand_faith_1_drink.html"+location.hash+"'")
+
+            $('.part')
+            .text('～ドリンク～')
+            break;}
+            else{
+                $('.title').eq(0)
+                .text('ロゴマーク')
+                .css('font-weight','700')
+
+                $('.title').eq(1).children().eq(0)
+                .text('ちゃんと覚えてる？')
+                .css('white-space','unset')
+                .css('line-height','1.25')
+                .css('font-weight','700')
+
+                $('.title').eq(1).children().eq(1)
+                .css('margin-top','15px')
+
+                $('.title').eq(2)
+                .text('スタート')
+                .attr("onclick","location='brand_faith_1_drink.html"+location.hash+"'")
+
+                $('.part')
+                .text('～ドリンク～')
+                break;  
+            }
+}
+// // 設定：將語系存入暫存
+// // --------------------------------------------------
+// localStorage.
+// setItem('selected_langunage', location.hash.substr(1, 2))
+
+
+// 設定：滑鼠移到語言時logo動畫
+// --------------------------------------------------
+// $('.button').mouseenter(function () {
+//     $(this).parent().prev()
+//         .removeClass('animate__zoomInUp')
+//         .addClass('animate__jello')
+// })
+//     .mouseleave(function () {
+//         $(this).parent().prev()
+//             .removeClass('animate__jello')
+//     })
+
+// 還不確定要不要用
+// 設定：滑鼠點選語言時關閉語言視窗
+// --------------------------------------------------
+// $('.button').click(function () {
+//     $('.select_language')
+//         .removeClass('animate__bounceIn')
+//         .addClass('animate__zoomOut')
+//         localStorage.setItem('selected_langunage', location.hash.substr(1, 2))
+//     setTimeout(function () {
+//         $('.select_language').addClass('d-none')
+//     }, 1000)
+// })
+
+// !!!設定：hash=null時要跳出詢問語言
+// !!!切換語言小選項
+
+
+
+
+
+
+
+
+
